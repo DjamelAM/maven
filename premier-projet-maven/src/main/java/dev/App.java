5ec4a15e30f715bd4cd65b6712475a592849ca61
unknown <amraoui.djamel@gmail.com>
2018-07-16 13:57:36 +0200

    bouh

---
+++ premier-projet-maven/src/main/java/dev/App.java
@@ -0,0 +1,17 @@
+package dev;
+import com.github.lalyos.jfiglet.FigletFont;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	String asciiArt = FigletFont.convertOneLine("Hello World!");
+        System.out.println(asciiArt);
+    	System.out.println( "Hello World!" );
+    }
+}
