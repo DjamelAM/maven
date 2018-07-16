1d5feb57362ba9f6e795d64afe56ac9883365bf1
unknown <amraoui.djamel@gmail.com>
2018-07-16 16:42:06 +0200

    final

--- premier-projet-maven/src/main/java/dev/App.java
+++ premier-projet-maven/src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 import com.github.lalyos.jfiglet.FigletFont;
+import java.util.ResourceBundle;
 
 
 /**
@@ -10,8 +11,23 @@ public class App
 {
     public static void main( String[] args )
     {
-    	String asciiArt = FigletFont.convertOneLine("Hello World!");
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
         System.out.println(asciiArt);
-    	System.out.println( "Hello World!" );
+        
+    	String environnement = ResourceBundle.getBundle("application").getString(
+    			"environnement");
+    	String str = "";
+    	if( environnement.equals("DEV"))
+    	{
+    		str = "Bonjour oui";
+    	}
+    	else
+    	{
+    		str= "Aurevoir oui";
+    	}
+    	String asciiArt1 = FigletFont.convertOneLine(str);
+    	
+    			System.out.println(asciiArt1);
     }
 }
